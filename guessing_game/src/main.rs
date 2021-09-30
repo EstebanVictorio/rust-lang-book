854fe746174565651ac4b7a15dbac2ea24cad1c1
Esteban Victorio <34945296+EstebanVictorio@users.noreply.github.com>
2021-09-30 12:07:16 -0500

    feat: handle invalid input and add multiple guessing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,20 +4,35 @@ use std::io;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..11);
-    println!("Guess the number!");
-    println!("Please input your guess:");
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read guess :(");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("Your guess: {}", guess);
-    println!("Secret number: {}", secret_number);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Guess the number!");
+        println!("Please input your guess:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read guess :(");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input!");
+                continue;
+            }
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Your guess: {}", guess);
+                println!("Too small!")
+            }
+            Ordering::Greater => {
+                println!("Your guess: {}", guess);
+                println!("Too big!")
+            }
+            Ordering::Equal => {
+                println!("Your guess: {}", guess);
+                println!("Secret number: {}", secret_number);
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
