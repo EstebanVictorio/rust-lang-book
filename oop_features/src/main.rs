ba8fb7fe8c6dd6d8166fcddd96f951a49412e02b
Esteban Victorio <34945296+EstebanVictorio@users.noreply.github.com>
2023-05-22 14:38:41 -0700

    feat: add oop lesson modules

---
+++ oop_features/src/main.rs
@@ -0,0 +1,4 @@
+// In this lesson we'll explore Rust's object-oriented features
+// We'll start with a struct containing a collection of values and a value that summarizes the average of such values
+
+fn main() {}
